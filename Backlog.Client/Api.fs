8e2b9939e8d53bf00cb9f4c3ede602150d7c4204
wleev <wlee@isabot.net>
2016-05-12 23:46:42 +0900

    Minor refactoring of API wrapper.

--- Backlog.Client/Api.fs
+++ Backlog.Client/Api.fs
@@ -3,44 +3,44 @@
 open Models
 open Net
 
-type Http = 
-    static member Request ( url: string, requestType : RequestType, ?data: seq<string * string>) =
-        let dataVal = match data with Some d -> d | None -> Seq.empty<string * string>
+type Http =
+    static member Request(url : string, requestType : RequestType, ?data : seq<string * string>) =
+        let dataVal =
+            match data with
+            | Some d -> d
+            | None -> Seq.empty<string * string>
         match requestType with
-        | Get -> Some ( HttpGet url )
-        | Delete -> Some ( HttpDelete url )
-        | Post -> Some ( HttpPost url dataVal)
-        | Patch -> Some ( HttpPatch url dataVal)
-        | _ -> None
+        | Get -> Some(HttpGet url)
+        | Delete -> Some(HttpDelete url)
+        | Post -> Some(HttpPost url dataVal)
+        | Patch -> Some(HttpPatch url dataVal)
 
-    
-
-module Users = 
+module Users =
     let baseUrl = "https://boop.backlogtool.com/api/v2/users"
-    
-    let getUsers () =
-        match Http.Request(baseUrl, Get) with 
+
+    let getUsers() =
+        match Http.Request(baseUrl, Get) with
         | Some response -> HttpBodyToEntity<list<User>> response.Body
         | None -> Some []
 
-    let getUserById (id:int) =
+    let getUserById (id : int) =
         let getUrl = baseUrl + "/" + id.ToString()
         match Http.Request(getUrl, Get) with
-         | Some response -> HttpBodyToEntity<User> response.Body
-         | None -> None
+        | Some response -> HttpBodyToEntity<User> response.Body
+        | None -> None
 
-    let addUser (user:User) =
+    let addUser (user : User) =
         match Http.Request(baseUrl, Post, user.ToAddFormValues) with
         | Some response -> HttpBodyToEntity<User> response.Body
         | None -> None
 
-    let updateUser (user:User) =
+    let updateUser (user : User) =
         let updateUrl = baseUrl + "/" + user.Id.ToString()
         match Http.Request(updateUrl, Patch, user.ToUpdateFormValues) with
         | Some response -> HttpBodyToEntity<User> response.Body
         | None -> None
 
-    let deleteUser (id:int) =
+    let deleteUser (id : int) =
         let deleteUrl = baseUrl + "/" + id.ToString()
         match Http.Request(deleteUrl, Delete) with
         | Some response -> response.StatusCode.Equals 200
